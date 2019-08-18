77bc6a68a6f1e799cc000c8f48ef1e8ed87a92b6
Sebastian Ferrari <sferrari@gmail.com>
2019-08-17 20:56:02 -0500

    Adding dark mode images

--- portfolio.php
+++ portfolio.php
@@ -8,7 +8,7 @@ include 'header.html';
     <div class="jumbotron">
       <div class="container">
         <h1>Portfolio</h1>
-        <p>I am a Senior Product Designer working on the Target mobile apps and website. I previously worked at <a href="https://www.nerdery.com/">The Nerdery</a> and Enlighten (since acquired by <a href="https://www.perficientdigital.com/">Perficient Digital</a>). I have a Master of Science in Information from the <a href="http://si.umich.edu">University of Michigan School of Information</a>.</p>
+        <p>I am a Senior Product Designer working on the Target mobile app and website. I previously worked at <a href="https://www.nerdery.com/">The Nerdery</a> and Enlighten (since acquired by <a href="https://www.perficientdigital.com/">Perficient Digital</a>). I have a Master of Science in Information from the <a href="http://si.umich.edu">University of Michigan School of Information</a>.</p>
       </div>
     </div>
 
@@ -17,16 +17,16 @@ include 'header.html';
 
         <div class="card">
           <h3 class="card-header">Target</h3>
-          <a href="target.php"><img class="card-img" src="static/target.png" alt="Target logo"></a>
+          <a href="target.php"><img class="card-img img-light" src="static/target@2x.png" alt="Target logo"><img class="card-img img-dark" src="static/target_dark@2x.png" alt="Target logo"></a>
           <div class="card-body">
-            <p class="card-text">I currently work on the Target mobile apps and website.</p>
+            <p class="card-text">I currently work on the Target mobile app and website.</p>
           <a class="btn-link btn-link-sm" href="target.php">View details</a>
           </div>
         </div>
 
         <div class="card">
           <h3 class="card-header">The Nerdery</h3>
-          <a href="nerdery.php"><img class="card-img" src="static/nerdery.png" alt="Nerdery logo"></a>
+          <a href="nerdery.php"><img class="card-img img-light" src="static/nerdery@2x.png" alt="Nerdery logo"><img class="card-img img-dark" src="static/nerdery_dark@2x.png" alt="Nerdery logo"></a>
           <div class="card-body">
             <p class="card-text">I worked at The Nerdery, a custom software development company, from June 2015 to May 2018.</p>
           <a class="btn-link btn-link-sm" href="nerdery.php">View details</a>
@@ -35,7 +35,7 @@ include 'header.html';
 
         <div class="card">
           <h3 class="card-header">Enlighten</h3>
-          <a href="enlighten.php"><img class="card-img" src="static/enlighten.png" alt="Enlighten logo"></a>
+          <a href="enlighten.php"><img class="card-img img-light" src="static/enlighten@2x.png" alt="Enlighten logo"><img class="card-img img-dark" src="static/enlighten_dark@2x.png" alt="Enlighten logo"></a>
           <div class="card-body">
             <p class="card-text">I worked part-time at Enlighten, a digital agency, while studying HCI.</p>
           <a class="btn-link btn-link-sm" href="enlighten.php">View details</a>
@@ -44,7 +44,7 @@ include 'header.html';
 
         <div class="card">
           <h3 class="card-header">UMSI</h3>
-          <a href="umsi.php"><img class="card-img" src="static/umlogo.png" alt="Logo of University of Michigan"></a>
+          <a href="umsi.php"><img class="card-img img-light" src="static/umlogo@2x.png" alt="Logo of University of Michigan"><img class="card-img img-dark" src="static/umlogo_dark@2x.png" alt="Logo of University of Michigan"></a>
           <div class="card-body">
             <p class="card-text">I finished my Master's degree focusing on Human-Computer Interaction in 2015.</p>
           <a class="btn-link btn-link-sm" href="umsi.php">View details</a>
