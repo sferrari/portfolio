a416997bc249311adb7deba2282719c5e6bda026
Sebastian Ferrari <sferrari@gmail.com>
2019-08-17 20:56:02 -0500

    Adding dark mode images

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include 'header.html';
           document.write(function(){Now=new Date;Hour=Now.getHours();5>Hour?msg="<h1 class='lg-h'>Hello there, night owl.</h1>":12>Hour?msg="<h1 class='lg-h'>Good morning.</h1>":18>Hour?msg="<h1 class='lg-h'>Good afternoon.</h1>":24>Hour&&(msg="<h1 class='lg-h'>Good evening.</h1>");return msg}());
       </script>
         <noscript><h1>Hello!</h1></noscript>
-        <p>My goal is to make software at scale that helps people in their everyday lives. This requires aligning strategy, research, and design around the core problem of understanding and serving people's needs - everything else follows. I enjoy working within teams that are dedicated to craftsmanship and continual learning.</p>
+        <p>My goal is to make software that helps people in their everyday lives. This requires aligning strategy, research, and design around the core problem of understanding and serving people's needs - everything else follows. I enjoy working within teams that are dedicated to craftsmanship, learning, and mutual respect.</p>
         <div style="margin-bottom:1em"><a type="button" class="btn-link" style="margin-bottom:5px; margin-right:25px; text-decoration:underline;" href="portfolio.php" role="link">View Portfolio</a>
         <a type="button" class="btn-link" style="margin-bottom:5px; text-decoration:underline;" href="/blog" role="link">Read Blog</a></div>
        </div>
@@ -21,7 +21,7 @@ include 'header.html';
        <div class="gridwrapper3">
        	  <div>
 	          <h2 class="box">Understand</h2>
-	          <p class="box">I enjoy conducting research to understand people's mental models and the job-to-be-done of a product. This work sets the foundation for a product's success - and I just like figuring out what makes people tick.</p>
+	          <p class="box">I conduct research to understand people's mental models and the job-to-be-done of a product. This work sets the foundation for a product's success - and I just like figuring out what makes people tick.</p>
           </div>
 
        	  <div>
